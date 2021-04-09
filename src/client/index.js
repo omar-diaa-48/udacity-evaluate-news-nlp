583fb3bba64cfe4aff72b20707aef2dff2d6e704
Omar Diaaeldine Elwakeel <67291253+omar-diaa-48@users.noreply.github.com>
2021-04-09 23:42:14 +0200

    view done

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+
+import {handleSubmit} from './js/handleSubmit';
+
+import './styles/body.scss';
+
+export {handleSubmit}
--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -6,7 +6,7 @@ function handleSubmit(event) {
 
     if(nlpText == null || nlpText.length == 0 || typeof nlpText !== "string")
     {
-        nlpResult.innerHTML = "Invalid submit"    
+        nlpResult.innerHTML = "Empty input is an invalid submit"    
     }
 
     fetch("http://localhost:8055/", 
@@ -22,12 +22,16 @@ function handleSubmit(event) {
             })
             .then(res => {
                 let result = '';
-                for (const item of res.sentimented_entity_list) {
-                    result += `Word ${item.form} is a ${item.type.split('>').pop()}\n`;
-                }
+
+                if(res.sentimented_entity_list == null)
+                   result = 'Not a valid text, should contain city or a country'; 
+                else
+                    for (const item of res.sentimented_entity_list) {
+                        result += `Word ${item.form} is a ${item.type.split('>').pop()}\n`;
+                    }
                 nlpResult.innerHTML = result;
             })
             .catch(err => console.log(err));
 }
 
-// export {handleSubmit}
+export {handleSubmit}
